4aae1ef6c70d568cdbebf4116f7e189224e1ef33
Lee Wai Kin <leewaikin19@gmail.com>
2025-01-23 00:20:12 +0800

    Add echo function

--- src/main/java/Glados.java
+++ src/main/java/Glados.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Glados {
     public static void main(String[] args) {
         System.out.println("Ah, thou art here! I am Glados, at thy service "
                 + "- if such a thing may be called service.\n"
                 + "How may I, in my immeasurable greatness, "
                 + "deign to assist thee, thou artless hedge-born scullion?\n");
-        System.out.println("Begone, thou pribbling, ill-nurtured knave!"
-                + "I care not when our paths cross again, for I hold thy presence in contempt!\n");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Begone, thou pribbling, ill-nurtured knave!"
+                        + "I care not when our paths cross again, "
+                        + "for I hold thy presence in contempt!\n");
+                break;
+            }
+            System.out.println(userInput);
+        }
+        scanner.close();
     }
 }
