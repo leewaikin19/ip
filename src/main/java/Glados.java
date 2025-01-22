89071f0ec403add012427fc469f0a1b638ecbb18
Lee Wai Kin <leewaikin19@gmail.com>
2025-01-23 00:11:27 +0800

    Rename Glados.java

---
+++ src/main/java/Glados.java
@@ -0,0 +1,10 @@
+public class Glados {
+    public static void main(String[] args) {
+        System.out.println("Ah, thou art here! I am Glados, at thy service "
+                + "- if such a thing may be called service.\n"
+                + "How may I, in my immeasurable greatness, "
+                + "deign to assist thee, thou artless hedge-born scullion?\n");
+        System.out.println("Begone, thou pribbling, ill-nurtured knave!"
+                + "I care not when our paths cross again, for I hold thy presence in contempt!\n");
+    }
+}
