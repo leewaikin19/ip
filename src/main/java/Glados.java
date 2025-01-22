6edfb95724807d099734f9b8fdd5bb1491aeb6f6
Lee Wai Kin <leewaikin19@gmail.com>
2025-01-23 00:30:23 +0800

    Add store list function

--- src/main/java/Glados.java
+++ src/main/java/Glados.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Glados {
+    private static ArrayList<String> items = new ArrayList<String>();
+
     public static void main(String[] args) {
+        
         System.out.println("Ah, thou art here! I am Glados, at thy service "
                 + "- if such a thing may be called service.\n"
                 + "How may I, in my immeasurable greatness, "
@@ -14,8 +18,14 @@ public class Glados {
                         + "I care not when our paths cross again, "
                         + "for I hold thy presence in contempt!\n");
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < items.size(); i++){
+                    System.out.println((i + 1) + ". " + items.get(i));
+                }
+            } else {
+                items.add(userInput);
+                System.out.println("added: " + userInput);
             }
-            System.out.println(userInput);
         }
         scanner.close();
     }
