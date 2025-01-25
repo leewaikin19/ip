13e13e0e1da68c58b27bd9943e6408b6d0ea760d
Lee Wai Kin <leewaikin19@gmail.com>
2025-01-25 23:33:45 +0800

    Add find feature

--- src/main/java/glados/commands/Command.java
+++ src/main/java/glados/commands/Command.java
@@ -1,4 +1,5 @@
 package glados.commands;
+
 import glados.local.Storage;
 import glados.tasks.TaskList;
 import glados.ui.Ui;
@@ -6,6 +7,7 @@ import glados.ui.Ui;
 public class Command {
     protected Boolean isExit = false;
     protected String command;
+    protected String query;
 
     public Command(String command) {
         this.command = command;
@@ -13,10 +15,16 @@ public class Command {
             isExit = true;
         }
     }
+
+    public Command(String command, String query) {
+        this.command = command;
+        this.query = query;
+    }
+
     public String getCommand() {
         return command;
     }
-    
+
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         switch (command) {
             case "exit":
@@ -24,10 +32,19 @@ public class Command {
             case "list":
                 Ui.show(tasks.toString());
                 break;
+            case "find":
+                TaskList queryResults = tasks.find(query);
+                if (queryResults.size() == 0) {
+                    Ui.show("There are no search results. Please try again.");
+                } else {
+                    Ui.show(queryResults.toString());
+                }
+                break;
             default:
                 break;
         }
     }
+
     public Boolean isExit() {
         return isExit;
     }
--- src/main/java/glados/tasks/TaskList.java
+++ src/main/java/glados/tasks/TaskList.java
@@ -1,8 +1,10 @@
 package glados.tasks;
+
 import java.util.ArrayList;
 
 public class TaskList {
     private ArrayList<Task> taskList;
+
     public TaskList() {
         taskList = new ArrayList<Task>();
     }
@@ -10,6 +12,7 @@ public class TaskList {
     public TaskList(ArrayList<Task> taskList) {
         this.taskList = taskList;
     }
+
     public int size() {
         return taskList.size();
     }
@@ -21,13 +24,30 @@ public class TaskList {
     public void remove(int index) {
         taskList.remove(index);
     }
+
     public void add(Task task) {
         taskList.add(task);
     }
 
+    /**
+     * Returns a task list with descriptions containing the query
+     * 
+     * @param query search query
+     * @return TaskList list of tasks that matches query
+     */
+    public TaskList find(String query) {
+        ArrayList<Task> result = new ArrayList<>();
+        for (Task task : taskList) {
+            if (task.description.contains(query)) {
+                result.add(task);
+            }
+        }
+        return new TaskList(result);
+    }
+
     public String toString() {
         String result = "";
-        for (int i = 0; i < taskList.size(); i++){
+        for (int i = 0; i < taskList.size(); i++) {
             result += (i + 1) + ". " + taskList.get(i) + "\n";
         }
         return result;
